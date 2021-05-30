ad9058781988652183a360fe1e615930eac3f86c
bhupendra05 <69967675+bhupendra05@users.noreply.github.com>
2021-05-30 13:29:48 +0530

    Update Salary_Predictor.py

--- Salary_Predictor.py
+++ Salary_Predictor.py
@@ -1,10 +1,11 @@
 import pandas
 
-ds=pandas.read_csv(Salary_data.csv)
+ds=pandas.read_csv("SalaryData.csv")
 x=ds['YearsExperience'].values.reshape(-1,1)
 y=ds['Salary']
 
 from sklearn.linear_model import LinearRegression
+model=LinearRegression()
 model=model.fit(x,y)
 
 import joblib
